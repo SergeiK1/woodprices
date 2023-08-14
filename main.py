5042803f790840f925af90ac5c4a10550e7070da
SergeiK1 <sergeikdr1@gmail.com>
2023-08-14 12:22:49 -0500

    original ideas

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-Test
+import tabula 
+
+
+
 
 # Reading PDF and converting to usable data __________________________________________
 
 # Installed java
+
+pdf_path = 'testfile.pdf'
+dfs = tabula.read_pdf(pdf_path, pages="all", multiple_tables=True)
+
+print(dfs)
 
 
 # Uplaoding this data into a seperate file to act as a database for the historical data _____________________________
@@ -23,3 +31,11 @@ Test
 
 # Possible additional functionality ____________________________
 # adds checks to see if price rose or dropped and make the chart different colors (green red) and make that able to be manipulated by the user 
+
+
+
+
+
+
+
+
